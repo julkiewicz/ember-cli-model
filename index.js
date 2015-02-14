16652ff0706de9ba8eacac182c165c47b158a62d
julkiewicz <julek.kopczewski@gmail.com>
2015-02-15 00:27:15 +0100

    Dalsze zmiany w ember-addon.

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-model'
+  name: 'ember-cli-model',
+
+  included: function(app) {
+    this._super.included(app);
+    
+    app.import(app.bowerDirectory + '/ember-model/ember-model.js');
+  }
 };
